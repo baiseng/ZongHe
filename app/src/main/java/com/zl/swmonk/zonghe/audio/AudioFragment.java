30c71b072650c873465ed9cc0254a5c20677baad
baiseng <swmonk@qq.com>
2019-02-13 14:47:37 +0800

    包含内涵图片以及搞笑视频

--- app/src/main/java/com/zl/swmonk/zonghe/audio/AudioFragment.java
+++ app/src/main/java/com/zl/swmonk/zonghe/audio/AudioFragment.java
@@ -4,7 +4,6 @@ import com.zl.swmonk.zonghe.R;
 import com.zl.swmonk.zonghe.base.ui.BaseFragment;
 
 public class AudioFragment extends BaseFragment {
-    private String hello;
     @Override
     protected int getContentLayoutId() {
         return R.layout.fragment_audio;
