6051d5eb6779427531823cf734d569e03b6d36e7
baiseng <swmonk@qq.com>
2019-02-13 14:44:53 +0800

    bug分支添加hello

--- app/src/main/java/com/zl/swmonk/zonghe/audio/AudioFragment.java
+++ app/src/main/java/com/zl/swmonk/zonghe/audio/AudioFragment.java
@@ -4,6 +4,7 @@ import com.zl.swmonk.zonghe.R;
 import com.zl.swmonk.zonghe.base.ui.BaseFragment;
 
 public class AudioFragment extends BaseFragment {
+    private String hello;
     @Override
     protected int getContentLayoutId() {
         return R.layout.fragment_audio;
